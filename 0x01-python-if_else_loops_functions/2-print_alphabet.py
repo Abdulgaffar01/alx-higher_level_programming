69fffe41d965231c0e794883f1aa930d249c1fde
Abdulgaffar <abdulrasaqabdulgaffar492@gmail.com>
2022-11-29 04:51:07 -0800

    first attempt to the task

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/ython3
+for i in range(87, 120):
+    print("{}".format(chr(i)), end=' ')
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_last_digit(numbers):
+    if numbers < 0:
+        numbers = numbers * -1
+    num = number % 10:
+        print("{}".format(num), emd='')
+        return num
