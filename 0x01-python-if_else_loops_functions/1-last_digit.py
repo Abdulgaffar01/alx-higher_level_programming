f4e30477ba703c8abf9a4fdd53dabef3000da781
Abdulgaffar <abdulrasaqabdulgaffar492@gmail.com>
2022-11-30 08:41:00 -0800

    Abdulgaffar

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 import random
-number = random(-10000, 10000)
-if number > 0:
-    num = number * -1
-    num = num % 5
-    num = num * -1
+number = random.randint(-10000, 10000)
+digit = abs(number) % 10
+if number < 0:
+    digit = -digit
+print(f"Last digit of {number:d} is {digit:d} and is ", end="")
+if digit > 5:
+    print("greater than 5")
+elif digit == 0:
+    print("0")
 else:
-    num = number % 10
-    print(f"Last digit of {}".format(number), end=' ')
-    if num > 5:
-        print(f"is {} and is greater than 5".format(num))
-    elif num == 0:
-        print(f"is {} is zero".format(num))
-    elif num < 6:
-        print(f"is {} and is less than 6 and not 0".format(num))
+    print("less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
+"""Print numbers 0 to 98 in decimal and hexadecimal."""
 for i in range(0, 98):
-    print("{} = {}".format(i), hex(i))
-    
+    print("{} = {}".format(i, hex(i))) 
