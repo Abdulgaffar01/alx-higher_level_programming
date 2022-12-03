ee5c93869987334e0d89f7c6adc6a8ec0ba7a042
Abdulgaffar <abdulrasaqabdulgaffar492@gmail.com>
2022-12-02 21:26:00 -0800

    Second attempt on this task

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
+    
     """Print he sum, difference, product, division
     of two integer like 10 and 5."""
     
@@ -8,6 +9,7 @@ if __name__ == "__main__":
 
     a = 10
     b = 5
+    
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
 
     """Print the count and list of args on different line"""
+    
     import sys
-    len = len(sys.argv) - 1
-    if len == 0:
+    
+       count = len(sys.argv) - 1
+       
+    if count == 0:
+        
         print("0 arguments.")
-    elif len == 1:
+        
+    elif count == 1:
+        
         print("1 argument:")
+        
     else:
-        print("{} arguments:".format((len)))
-    for i in range(1, len + 1):
-        print("{}: {}".format(i, sys.argv[i]))
+        
+        print("{} arguments:".format(count))
+        
+    for i in range(count):
+        
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
