bb5f90053bcd199270d33436ba958815692a5dc4
Abdulgaffar <abdulrasaqabdulgaffar492@gmail.com>
2022-12-03 01:34:13 -0800

    First attempt on this task

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(string):
+    result = []
+    for letter in my_string:
+        if letter != 'c' and letter is != 'C':
+            result.append(letter)
+        return ''.join(result)
